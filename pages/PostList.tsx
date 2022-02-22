5807b43f11c2e838251b91c67591d4d3a87b79a4
gkdfo40 <gkdfo40@gmail.com>
2022-02-23 01:57:09 +0900

    Add pages/ PostList.tsx

---
+++ pages/PostList.tsx
@@ -0,0 +1,32 @@
+import { GetStaticProps } from "next";
+import Link from "next/link";
+
+type Post = { filename: string }
+
+const PostList = ({ posts }: { posts: Post[] }) => {
+    return (
+        <div>
+            {posts.map((post, index) => (
+                <Link key={index} href="/videos/[id]" as={`/videos/${post.filename}`}>
+                    <a>watch {post.filename}</a>
+                </Link>
+            ))}
+        </div>
+    )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+    // Call an external API endpoint to get posts
+    const res = await fetch('http://localhost:5000/api/videos/posts')
+    const posts = await res.json()
+
+    // By returning { props: { posts } }, the Blog component
+    // will receive `posts` as a prop at build time
+    return {
+        props: {
+            posts,
+        },
+    }
+}
+
+export default PostList;
