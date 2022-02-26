070189e3aa4dfd9ecf39ec412eff68224ce49f6e
gkdfo40 <gkdfo40@gmail.com>
2022-02-26 14:52:06 +0900

    Add Video List Nav bar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import VideoUploader from '../components/VideoUploader'
 import styles from '../styles/Home.module.css'
 
@@ -17,6 +18,11 @@ const Home: NextPage = () => {
         <VideoUploader />
       </main>
       <nav>
+        <ul>
+          <Link href="/PostList" >
+            <a><li>video List</li></a>
+          </Link>
+        </ul>
       </nav>
     </div>
   )
